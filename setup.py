d0b28e9b37a497448e8f6c36b50f17daebb1aeb2
Richelle Shim <richelleshim@gmail.com>
2024-12-04 13:22:58 -0800

    lit 🎵 Commit made while listening to: Touch The Sky by Kanye West, Lupe Fiasco

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/richelleshim/spotify-commits",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
         "spotipy",
         "python-dotenv",
